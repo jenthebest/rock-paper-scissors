b38b414cb9b8df10f6553ad8fb451addc0955b89
Jen Best <jenthebest@gmail.com>
2015-02-17 12:54:14 -0500

    added form input

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?")
-var computerChoice = Math.random();
+var result = document.getElementById("result");
+var rpsInput;
+var userChoice;
 
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if (computerChoice < 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
+function init() {
+	// reference to button using button's id
+	var playButton = document.getElementById("playButton");
+	playButton.onclick = handlePlayButton;
 }
 
-alert("The computer chooses " + computerChoice);
+function handlePlayButton() {
+	// code to get the value from the form
+	rpsInput = document.getElementById("rpsInput");
+	userChoice = rpsInput.value.toLowerCase();
+	
+	var computerChoice = Math.random();
+	if (computerChoice < 0.34) {
+		computerChoice = "rock";
+	} else if (computerChoice < 0.67) {
+		computerChoice = "paper";
+	} else {
+		computerChoice = "scissors";
+	}
 
-var compare = function(choice1, choice2) {
-	if (choice1 === choice2) {
-		alert("The resule is a tie!");
-	} else if (choice1 === "rock") {
-		if (choice2 === "scissors") {
-			alert("rock wins");
-		} else {
-			alert("paper wins");
-		}
-	} else if (choice1 === "paper") {
-		if (choice2 === "rock") {
-			alert("paper wins");
-		} else {
-			alert("scissors wins");
-		}
-	} else if (choice1 === "scissors") {
-		if (choice2 === "rock") {
-			alert("rock wins");
-		} else {
-			alert("scissors wins");
+	var computer = document.getElementById("computer");
+	computer.innerHTML = "The computer chooses " + computerChoice;
+
+	function compare(choice1, choice2) {
+		if (choice1 === choice2) {
+			result.innerHTML = "The result is a tie!";
+		} else if (choice1 === "rock") {
+			if (choice2 === "scissors") {
+				result.innerHTML = "You win!";
+			} else {
+				result.innerHTML = "Paper wins!";
+			}
+		} else if (choice1 === "paper") {
+			if (choice2 === "rock") {
+				result.innerHTML = "You win!";
+			} else {
+				result.innerHTML = "Scissors wins!";
+			}
+		} else if (choice1 === "scissors") {
+			if (choice2 === "rock") {
+				result.innerHTML = "Rock wins!";
+			} else {
+				result.innerHTML = "Scissors wins!";
+			}
 		}
 	}
+	compare(userChoice, computerChoice);
 }
 
-compare(userChoice, computerChoice);
+window.onload = init;
