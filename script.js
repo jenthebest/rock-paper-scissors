2b2bff56252952e610f5adff85e9702435aad051
Jen Best <jenthebest@gmail.com>
2015-02-21 15:10:14 -0500

    refactored

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var result = document.getElementById("result");
 var rpsInput;
 var userChoice;
+var computerChoice;
 
-function init() {
+window.onload = function() {
 	// reference to button using button's id
 	var playButton = document.getElementById("playButton");
 	playButton.onclick = handlePlayButton;
@@ -13,7 +14,7 @@ function handlePlayButton() {
 	rpsInput = document.getElementById("rpsInput");
 	userChoice = rpsInput.value.toLowerCase();
 	
-	var computerChoice = Math.random();
+	computerChoice = Math.random();
 	if (computerChoice < 0.34) {
 		computerChoice = "rock";
 	} else if (computerChoice < 0.67) {
@@ -22,33 +23,34 @@ function handlePlayButton() {
 		computerChoice = "scissors";
 	}
 
+	compare(userChoice, computerChoice);
 	var computer = document.getElementById("computer");
 	computer.innerHTML = "The computer chooses " + computerChoice;
-
-	function compare(choice1, choice2) {
-		if (choice1 === choice2) {
-			result.innerHTML = "The result is a tie!";
-		} else if (choice1 === "rock") {
-			if (choice2 === "scissors") {
-				result.innerHTML = "You win!";
-			} else {
-				result.innerHTML = "Paper wins!";
-			}
-		} else if (choice1 === "paper") {
-			if (choice2 === "rock") {
-				result.innerHTML = "You win!";
-			} else {
-				result.innerHTML = "Scissors wins!";
-			}
-		} else if (choice1 === "scissors") {
-			if (choice2 === "rock") {
-				result.innerHTML = "Rock wins!";
-			} else {
-				result.innerHTML = "Scissors wins!";
-			}
-		}
-	}
-	compare(userChoice, computerChoice);
 }
 
-window.onload = init;
+
+
+function compare(choice1, choice2) {
+	if (choice1 === choice2) {
+		result.innerHTML = "The result is a tie!";
+	} else if (choice1 === "rock") {
+		if (choice2 === "scissors") {
+			result.innerHTML = "You win! :)";
+		} else {
+			result.innerHTML = "The computer wins :(";
+		}
+	} else if (choice1 === "paper") {
+		if (choice2 === "rock") {
+			result.innerHTML = "You win! :)";
+		} else {
+			result.innerHTML = "The computer wins :(";
+		}
+	} else if (choice1 === "scissors") {
+		if (choice2 === "rock") {
+			result.innerHTML = "The computer wins :(";
+		} else {
+			result.innerHTML = "You win! :)";
+		}
+	}
+}
+
